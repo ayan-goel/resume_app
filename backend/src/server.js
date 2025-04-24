bcc611a1b9d6c9102e0dd6aa86f9046722b309d4
ayan-goel <agoel320@gatech.edu>
2025-04-24 00:32:52 -0400

    more changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,10 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://gtakpsi-resume-app.vercel.app', 'https://gtakpsi-resume-app-backend.org'] 
-    : 'http://localhost:3000',
-  credentials: true
+  origin: ['https://gtakpsi-resume-app.vercel.app', 'http://localhost:3000'],
+    credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
