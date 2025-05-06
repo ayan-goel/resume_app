57d3f1774041db878a53f7bb5eec7fcea3c9dc70
ayan-goel <agoel320@gatech.edu>
2025-05-05 20:02:17 -0400

    frontend sync

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://gtakpsi-resume-app-backend.org/api',
   headers: {
     'Content-Type': 'application/json',
   },
